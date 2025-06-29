dccc222006c89ebc142e4e500e4db1ae9ec3560c
gkkconan <conan.gkk@gmail.com>
2025-06-29 18:34:22 +0200

    feat(vite-env.d.ts): add svgr module to import svg as react components by appending  to the import path

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+// NOTE: import svgs as react components by appending `?react` to the import path
+declare module '*.svg?react' {
+  import * as React from 'react';
+  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
