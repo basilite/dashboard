1cddd3d794ebe6aa6337121f0d554a799085525d
gkkconan <conan.gkk@gmail.com>
2025-07-25 13:06:01 +0200

    feat(pages/notFound): add styles and go back btn

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,13 @@
+import styles from '../css/notFound.module.css';
+import { useNavigate } from 'react-router-dom';
+
 export default function notFoundPage(){
-    return <h1>404 Not Found</h1>;
+    const navigate = useNavigate();
+
+    return (
+        <section>
+            <h1 className={styles.notFoundText}>404 Not Found</h1>
+            <button className={styles.goBack} onClick={() => navigate(-1)}> Go Back </button>
+        </section>
+    );
 }
